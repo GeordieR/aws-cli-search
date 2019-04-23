00a37aa5b090f288badf07712da3c8fc646bc2ff
Geordie Rogers <geordie@grogers.xyz>
2019-04-23 19:33:22 +1200

    Allow navigation to commands and sub-commands

--- background.js
+++ background.js
@@ -1,7 +1,22 @@
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
-    // For now just open the docs homepage
-    var newURL = 'https://docs.aws.amazon.com/cli/latest/reference/'
-    chrome.tabs.create({ url: newURL });
-  });
+
+    // Decode the provided string
+    text = decodeURI(text)
+    // Split the strings separating each command
+    var commands = text.split(' ')
+    
+    if(commands.length == 1){
+      // Open the documentation for a command
+      var newURL = 'https://docs.aws.amazon.com/cli/latest/reference/'+commands[0]+'/'
+    } else {
+      // Open the documentation for a sub-command
+      var newURL = 'https://docs.aws.amazon.com/cli/latest/reference/'+text.replace(" ","/")+'.html'
+    }
+
+    // Open the URL in the current window
+    chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
+      chrome.tabs.update(tab.id, {url: newURL});
+    })
+  })
